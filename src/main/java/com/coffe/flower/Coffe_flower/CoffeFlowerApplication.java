b3c2e7a5d99eb099c4443ae63fa1067eec88c291
PV-Alumno <PV-Alumno@duoc.acad>
2025-11-06 09:38:31 -0300

    first commit

---
+++ src/main/java/com/coffe/flower/Coffe_flower/CoffeFlowerApplication.java
@@ -0,0 +1,13 @@
+package com.coffe.flower.Coffe_flower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeFlowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeFlowerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffe/flower/Coffe_flower/CoffeFlowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffe.flower.Coffe_flower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeFlowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
