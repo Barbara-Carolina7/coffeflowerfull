216871cf31477812ccb6b66db752be61fdab36ea
PV-Alumno <PV-Alumno@duoc.acad>
2025-11-13 09:51:16 -0300

    llenando clases

--- src/main/java/com/coffe/flower/Coffe_flower/model/Categoria.java
+++ src/main/java/com/coffe/flower/Coffe_flower/model/Categoria.java
@@ -1,5 +1,29 @@
 package com.coffe.flower.Coffe_flower.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Categoria")
 public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nombre;
+    private String descripción;
+
+    public long getId(){ return id; }
+    public void setId(Long id ){ this.id = id; }
+
+    public long getNombre(){ return nombre; }
+    public void setNombre(String nombre ){ this.nombre = nombre; }
+
+    public long getDescripción(){ return descripción; }
+    public void setDescripción(String descripción ){ this.descripción = descripción; }
+
+
 
 }
